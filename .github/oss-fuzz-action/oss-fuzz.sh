012ca9841f47aba0a3a384baa0d1500a217dff8a
Jonathan Metzman <metzman@chromium.org>
2019-12-17 10:06:52 -0800

    fix

--- .github/oss-fuzz-action/oss-fuzz.sh
+++ .github/oss-fuzz-action/oss-fuzz.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+# This repo will be changed.
 git clone https://github.com/jonathanmetzman/oss-fuzz.git --depth 1 $GITHUB_WORKSPACE/oss-fuzz
 python3 $GITHUB_WORKSPACE/oss-fuzz/infra/helper.py cifuzz test-project
